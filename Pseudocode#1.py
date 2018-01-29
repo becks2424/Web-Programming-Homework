246ec1898e4e22c29c4daf1d2163e1bfeb02d2dc
Becky Menda <mrm1@graceland.edu>
2018-01-29 10:59:44 -0600

    website update

---
+++ Pseudocode#1.py
@@ -0,0 +1,40 @@
+#Rebecca Menda
+
+
+#this program will allow the doctor and nurse to stay in communication in order
+#to make sure the patients are being kept track of and to avoid
+#any misunderstandings.
+
+
+#Step1
+#The waiting room should be able to have a patient list directory where patients
+#that have already visited there can just check in by providing their first
+#and last name which the nurse can enter in the system and look up. They will also be
+#able to enter their birthdate in order to have accurate information
+#pulled up from the system.
+
+#Step2
+#After the patient checks in with the nurse, once they get to the exam room,
+#the doctor will be able to see what they're visiting for by entering their
+#name and birthdate in order to get the information entered by the nurse in the
+#waiting room.
+
+#Step 3
+#After the doctor sees the patient, they will enter the medication to be given
+#to the patient and send it back to the nurse using the birthdate as sort of a
+#passcode to make sure it is prescribed to the right patient.
+
+#Step 4
+#after the patient gets back to the waiting room, the nurse will be able
+#to tell that they already visited by just asking for their birthdate
+#and the information that will come up will consist of the prescription
+#given by the doctor, confirming that they have already visited with the doctor.
+
+#Step 5
+#once the patient gets their prescription from the nurse, she checks the name off
+#and puts them on the patients checked list
+#so that she can go back in anytime and double check just to make sure.
+
+#all this will be done using two different programs. One used by the nurse
+#the other by the doctor but they will both be linked to eachother
+#to make communication easier.
