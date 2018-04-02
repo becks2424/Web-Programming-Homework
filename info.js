abf756abee031bd253d8717884a5236d3f14defa
becks2424 <35306213+becks2424@users.noreply.github.com>
2018-04-02 12:11:23 -0500

    Add files via upload

---
+++ info.js
@@ -0,0 +1,6 @@
+var name = "Enter Name Here";
+        
+            document.write("<span id ='footer'>Rebecca's Website</span><br />");
+            document.write("<span id ='footer'>©2018 Rebecca Menda</span><br />");
+            document.write("<span id ='footer'>Modified Mon Apr 02 2018</span><br />");
+            document.write("<span id ='footer'>Retrieved: Mon Apr 02 2018 12:42 (Central Time Zone)</span><br />");
